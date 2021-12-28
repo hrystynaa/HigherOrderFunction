ca8592a1505a5fb67f8b119711be4f7bfa988e10
Hrystyna <yaskulskahrystyna@gmail.com>
2021-12-29 00:48:08 +0200

    Lab 8

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,21 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    const typeName = types[i];
+    const type = typeName.name.toLowerCase();
+    if (typeof arg !== type) {
+      throw new TypeError(`Arguments must be a ${type}!`);
+    }
+  }
+  const res = fn(...args);
+  const typeName = types[types.length - 1];
+  const type = typeName.name.toLowerCase();
+  if (typeof res !== type) {
+    throw new TypeError(`Result must be a ${type}!`);
+  }
+  return res;
+};
 
 module.exports = { contract };
